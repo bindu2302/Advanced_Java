912718d781eb4b9ab8ae9904e642a335af9e4333
HimaBindu Nara <naarahimabindu@gmail.com>
2025-03-21 12:42:41 +0530

    Add files via upload

---
+++ Exception_Handling/src/Null_pointer_exception.java
@@ -0,0 +1,7 @@
+public class Null_pointer_exception {
+	    public static void main(String[] args) {
+	        String str = null;
+	        System.out.println(str.length()); // Causes NullPointerException:Occurs when an object reference is accessed before being initialized.
+	    }
+	}
+
